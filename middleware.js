134fc76f6d69c770573b9afc0a3ba01a86c9b212
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2025-01-09 17:56:41 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,20 +2,20 @@ import { NextResponse } from 'next/server';
 import { getSession } from '@auth0/nextjs-auth0/edge';
 
 export async function middleware(req) {
-  const res = NextResponse.next();
-  const session = await getSession(req, res);
+//   const res = NextResponse.next();
+//   const session = await getSession(req, res);
 
-  if (!session) {
-    // Utilisateur non authentifié, redirigez vers la route de login Auth0
-    const loginUrl = new URL('/api/auth/login', req.url);
-    return NextResponse.redirect(loginUrl);
-  }
+//   if (!session) {
+//     // Utilisateur non authentifié, redirigez vers la route de login Auth0
+//     const loginUrl = new URL('/api/auth/login', req.url);
+//     return NextResponse.redirect(loginUrl);
+//   }
 
-  return res;
-}
+//   return res;
+// }
 
-export const config = {
-  matcher: [
-    "/((?!api/auth0|login|api/*|_next/static|_next/image|favicon.ico|.*\\..*$).*)",
-  ],
+// export const config = {
+//   matcher: [
+//     "/((?!api/auth0|login|api/*|_next/static|_next/image|favicon.ico|.*\\..*$).*)",
+//   ],
 };
